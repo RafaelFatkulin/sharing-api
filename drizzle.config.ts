5dc9251f9490f4f31fedc1d42521399c6a16b84e
RafaelFatkulin <armistman@vk.com>
2025-04-17 00:03:19 +0500

    fix date creating while creating a refresh token

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
-import {defineConfig} from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-    out: './migrations',
-    schema: ['./src/features/**/schema.ts'],
-    dialect: 'postgresql',
-    dbCredentials: {
-        url: process.env.DATABASE_URL!,
-    }
-})
+  out: "./migrations",
+  schema: ["./src/features/**/*.schema.ts"],
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
--- src/features/auth/auth.repository.ts
+++ src/features/auth/auth.repository.ts
@@ -3,31 +3,34 @@ import { refreshTokens } from "./auth.schema";
 import { eq } from "drizzle-orm";
 
 export class AuthRepository {
-    async createRefreshToken(userId: number, token: string) {
-        const [refreshToken] = await db.insert(refreshTokens).values({
-            userId,
-            token,
-            expiresAt: new Date(Date.now() * 7 * 25 * 60 * 60 * 1000)
-        }).returning()
+  async createRefreshToken(userId: number, token: string) {
+    const [refreshToken] = await db
+      .insert(refreshTokens)
+      .values({
+        userId,
+        token,
+        expiresAt: new Date(Date.now() + 7 * 25 * 60 * 60 * 1000),
+      })
+      .returning();
 
-        return refreshToken
-    }
+    return refreshToken;
+  }
 
-    async getRefreshToken(token: string) {
-        const [refreshToken] = await db
-            .select()
-            .from(refreshTokens)
-            .where(eq(refreshTokens.token, token))
+  async getRefreshToken(token: string) {
+    const [refreshToken] = await db
+      .select()
+      .from(refreshTokens)
+      .where(eq(refreshTokens.token, token));
 
-        return refreshToken
-    }
+    return refreshToken;
+  }
 
-    async deleteRefreshToken(token: string) {
-        const [refreshToken] = await db
-            .delete(refreshTokens)
-            .where(eq(refreshTokens.token, token))
-            .returning()
+  async deleteRefreshToken(token: string) {
+    const [refreshToken] = await db
+      .delete(refreshTokens)
+      .where(eq(refreshTokens.token, token))
+      .returning();
 
-        return refreshToken
-    }
+    return refreshToken;
+  }
 }
